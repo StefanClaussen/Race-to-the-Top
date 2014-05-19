01a9b1d7d2532844230f0bd1b38820290d15942f
StefenClausen <stefanclaussen@me.com>
2014-05-19 11:07:35 +0100

    Complete as of lecture 221

--- Race to the Top/STCSpaceShipView.m
+++ Race to the Top/STCSpaceShipView.m
@@ -21,6 +21,17 @@
 - (void)drawRect:(CGRect)rect
 {
     // Drawing code
+    UIBezierPath *bezierPath = [UIBezierPath bezierPath];
+    bezierPath.lineWidth = 2.0;
+    //Making it dynamic, 1/6th of width, and 1/3rd of height
+    [bezierPath moveToPoint:CGPointMake(1/6.0 * self.bounds.size.width, 1/3.0 * self.bounds.size.height)];
+    [bezierPath addLineToPoint:CGPointMake(1/6.0 *self.bounds.size.width, 2/3.0 * self.bounds.size.height)];
+    [bezierPath addLineToPoint:CGPointMake(5/6.0 *self.bounds.size.width, 2/3.0 * self.bounds.size.height)];
+    [bezierPath addLineToPoint:CGPointMake(2/3.0 *self.bounds.size.width, 1/2.0 * self.bounds.size.height)];
+    [bezierPath addLineToPoint:CGPointMake(1/3.0 *self.bounds.size.width, 1/2.0 * self.bounds.size.height)];
+    [bezierPath closePath];
+    //Need to call the stroke method in order to see the line.
+    [bezierPath stroke];
 }
 
 
