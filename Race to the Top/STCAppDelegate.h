3ebf1e2e51383fe4e93318670db67362a3edd5cd
StefenClausen <stefanclaussen@me.com>
2014-05-19 10:34:52 +0100

    Initial Commit

---
+++ Race to the Top/STCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STCAppDelegate.h
+//  Race to the Top
+//
+//  Created by Stefan Claussen on 19/05/2014.
+//  Copyright (c) 2014 One foot after the other. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
