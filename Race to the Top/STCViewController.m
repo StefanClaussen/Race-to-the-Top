666a563c7c1ba758913143980e52313140c726dc
StefenClausen <stefanclaussen@me.com>
2014-05-20 11:16:18 +0100

    Complete ending lecture 232

--- Race to the Top/STCViewController.m
+++ Race to the Top/STCViewController.m
@@ -1,14 +1,12 @@
 //
 //  STCViewController.m
 //  Race to the Top
-//
-//  Created by Stefan Claussen on 19/05/2014.
-//  Copyright (c) 2014 One foot after the other. All rights reserved.
-//
 
 #import "STCViewController.h"
+#import "STCPathView.h"
 
 @interface STCViewController ()
+@property (strong, nonatomic) IBOutlet STCPathView *pathView;
 
 @end
 
@@ -18,12 +16,22 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapDetected:)];
+    tapRecognizer.numberOfTapsRequired = 2;
+    [self.pathView addGestureRecognizer:tapRecognizer];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)tapDetected:(UITapGestureRecognizer *)tapRecognizer
+{
+    NSLog(@"Tapped");
+    CGPoint tapLocation = [tapRecognizer locationInView:self.pathView];
+    NSLog(@"Tap location is at (%f, %f)", tapLocation.x, tapLocation.y);
 }
 
 @end
