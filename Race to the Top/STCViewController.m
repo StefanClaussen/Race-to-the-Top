355e05493da263a05eeaf279d28dfc98968d44ca
StefenClausen <stefanclaussen@me.com>
2014-05-20 13:56:35 +0100

    Complete ending lecture 236

--- Race to the Top/STCViewController.m
+++ Race to the Top/STCViewController.m
@@ -6,8 +6,14 @@
 #import "STCPathView.h"
 #import "STCMountainPath.h"
 
+#define STCMAP_STARTING_SCORE 15000
+#define STCMAP_SCORE_DECREMENT_AMOUNT 100
+#define STCTIMER_INTERVAL 0.1
+#define STCWALL_PENALTY 500
+
 @interface STCViewController ()
 @property (strong, nonatomic) IBOutlet STCPathView *pathView;
+@property (strong, nonatomic) NSTimer *timer;
 
 @end
 
@@ -23,6 +29,9 @@
     
     UIPanGestureRecognizer *panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panDetected:)];
     [self.pathView addGestureRecognizer:panRecognizer];
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:STCTIMER_INTERVAL target:self selector:@selector(timerFired) userInfo:nil repeats:YES];
+    
+    self.scoreLabel.text = [NSString stringWithFormat:@"Score: %i", STCMAP_STARTING_SCORE];
 }
 
 - (void)didReceiveMemoryWarning
@@ -41,7 +50,7 @@
         UIBezierPath *tapTarget = [STCMountainPath tapTargetForPath:path];
         
         if ([tapTarget containsPoint:fingerLocation]){
-            NSLog(@"You hit the wall");
+            [self decrementScoreByAmount:STCWALL_PENALTY];
         }
     }
 }
@@ -51,6 +60,20 @@
     NSLog(@"Tapped");
     CGPoint tapLocation = [tapRecognizer locationInView:self.pathView];
     NSLog(@"Tap location is at (%f, %f)", tapLocation.x, tapLocation.y);
+}
+
+- (void)timerFired
+{
+    [self decrementScoreByAmount:STCMAP_SCORE_DECREMENT_AMOUNT];
+}
+
+- (void)decrementScoreByAmount:(int)amount
+{
+    NSString *scoreText = [[self.scoreLabel.text componentsSeparatedByString:@" "] lastObject];
+    int score = [scoreText intValue];
+    score = score - amount;
+    
+    self.scoreLabel.text = [NSString stringWithFormat:@"Score: %i", score];
 }
 
 @end
